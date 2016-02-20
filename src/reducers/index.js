a781267c4c4e7711efc0c2e8ab7d74401c2068a3
Henrique Ramos Limas <henrique.ramos.limas@gmail.com>
2016-02-20 12:36:04 -0200

    feat(reducer): create route reducer.

---
+++ src/reducers/index.js
@@ -0,0 +1,32 @@
+import {ROUTE_REQUEST, ROUTE_SUCCESS, ROUTE_ERROR} from '../constants';
+
+function currentRoute(state, action) {
+    return {
+        isChanging: action.type === ROUTE_REQUEST,
+        didInvalidate: action.type === ROUTE_ERROR,
+        uri: action.uri,
+        params: {}
+    };
+}
+
+export default function reducer(state, action) {
+    state = state || {
+        current: {}
+    };
+    switch(action.type) {
+        case ROUTE_REQUEST:
+            return {
+                current: currentRoute(state, action)
+            };
+        case ROUTE_SUCCESS:
+            return {
+                current: currentRoute(state, action)
+            };
+        case ROUTE_ERROR:
+            return {
+                current: currentRoute(state, action)
+            };
+        default:
+            return state;
+    }
+}
---
+++ test/reducers/index.spec.js
@@ -0,0 +1,104 @@
+import expect from 'expect';
+import reducer from '../../src/reducers';
+import {ROUTE_REQUEST, ROUTE_SUCCESS, ROUTE_ERROR} from '../../src/constants';
+
+describe('Reducer', function() {
+  context('when is an unknown action', function() {
+    it('should return the same state.', function() {
+      let action = {
+        type: 'UNKNOWN'
+      };
+
+      let currentState = {};
+
+      expect(reducer(currentState, action)).toBe(currentState);
+    });
+  });
+
+  it('should define a default state.', function() {
+    let action = {
+      type: 'UNKNOWN'
+    };
+
+    let expectedState = {
+      current: {}
+    };
+
+    expect(reducer(undefined, action)).toEqual(expectedState);
+  });
+
+  context('ROUTE_REQUEST', function() {
+    let action;
+
+    beforeEach(function() {
+      action = {
+        type: ROUTE_REQUEST,
+        uri: '/path'
+      }
+    });
+
+    it('should return the current state with a request state.', function() {
+      let currentState = {};
+      let expectedState = {
+        current: {
+          isChanging: true,
+          didInvalidate: false,
+          uri: '/path',
+          params: {}
+        }
+      };
+
+      expect(reducer(currentState, action)).toEqual(expectedState);
+    });
+  });
+
+  context('ROUTE_SUCCESS', function() {
+    let action;
+
+    beforeEach(function() {
+      action = {
+        type: ROUTE_SUCCESS,
+        uri: '/path'
+      }
+    });
+
+    it('should return the current state with a success state.', function() {
+      let currentState = {};
+      let expectedState = {
+        current: {
+          isChanging: false,
+          didInvalidate: false,
+          uri: '/path',
+          params: {}
+        }
+      };
+
+      expect(reducer(currentState, action)).toEqual(expectedState);
+    });
+  });
+
+  context('ROUTE_ERROR', function() {
+    let action;
+
+    beforeEach(function() {
+      action = {
+        type: ROUTE_ERROR,
+        uri: '/path'
+      }
+    });
+
+    it('should return the current state with an error state.', function() {
+      let currentState = {};
+      let expectedState = {
+        current: {
+          isChanging: false,
+          didInvalidate: true,
+          uri: '/path',
+          params: {}
+        }
+      };
+
+      expect(reducer(currentState, action)).toEqual(expectedState);
+    });
+  });
+});
