aa6c30ddd0da4436c711e1607a580a5c2b423675
Henrique Ramos Limas <henrique.ramos.limas@gmail.com>
2016-02-21 22:41:27 -0300

    feat(Main): add actions into public API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import * as Router from './router';
 import * as History from './history';
 import reducer from './reducers';
+import * as actions from './actions';
 
 module.exports = {
   History,
   Router,
-  reducer
+  reducer,
+  actions
 };
